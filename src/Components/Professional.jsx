94fa090199897497dc08bc12f1b84904b8d19236
varanasi.saipranav <varanasi.saipranav@gmail.com>
2025-06-12 15:27:16 +0000

    all changes

--- src/Components/Professional.jsx
+++ src/Components/Professional.jsx
@@ -3,7 +3,6 @@ import Header from "./header";
 import '../Styles/Professional.css';
 import Experience from "./Experience";
 import { TypeAnimation } from "react-type-animation";
-import Resume from "./Resume";
 
 export default function Professional() {
     return (
@@ -58,8 +57,7 @@ export default function Professional() {
                     <p>Implemented volume control using hand gesture detection with a camera. Platform: Python. Libraries: OpenCV, pycaw, Mediapipe, Numpy.</p>
                     <button onClick={() => window.open("https://github.com/your-volume-control", "_blank")}>View on GitHub</button>
                 </div>
-                <h1 style={{ fontFamily: "Sixtyfour Convergence", textAlign: "center" }}>Resume</h1>               
-                <Resume/>
+
 
                 <br />
             </div>
