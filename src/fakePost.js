86bafe50ad05d41a65984d5d2825261d6db1b247
varanasi.saipranav <varanasi.saipranav@gmail.com>
2025-06-16 16:19:27 +0000

    16-06-25 commit 1

--- src/fakePost.js
+++ src/fakePost.js
@@ -1,5 +1,5 @@
 import post1 from './Assets/post1.png';
-
+import post2 from './Assets/12333.avif'
 const fakePosts = [
     {
         image: post1,
@@ -7,6 +7,13 @@ const fakePosts = [
         date: 'May 13, 2025',
         url: 'https://medium.com/@varanasi.saipranav/dont-join-tcs-as-a-fresher-4be5f8fc1991',
         content: 'Hey This is a Medium post and it contains a story of a TCS Fresher.'
+    }
+    {
+        image: post2,
+        heading: 'Optimal Cup of Coffee.',
+        date: 'June 16, 2025',
+        url: 'https://medium.com/@varanasi.saipranav/optimal-cup-of-coffee-51a870ecbd2b',
+        content: 'Hey This is a Medium post and it has my views on Coffee.'
     }
 ];
 
